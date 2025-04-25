78c812e2c85ddac669f3b4d2b44a8e26332a6886
Hyochang098 <wlsgyckd7@naver.com>
2025-04-26 00:19:26 +0900

    feat: 댓글, 게시글 mapper작성

--- src/main/java/com/ssafy/ssafit/service/impl/CommentServiceImpl.java
+++ src/main/java/com/ssafy/ssafit/service/impl/CommentServiceImpl.java
@@ -2,10 +2,12 @@ package com.ssafy.ssafit.service.impl;
 
 import java.util.List;
 
+import org.springframework.stereotype.Service;
 import com.ssafy.ssafit.dto.CommentDto;
 import com.ssafy.ssafit.repository.CommentRepository;
 import com.ssafy.ssafit.service.CommentService;
 
+@Service
 public class CommentServiceImpl implements CommentService {
 
 	private final CommentRepository commentRepository;
